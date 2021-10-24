1f60b6e00371179d033d7b63dcc00bc90500a47e
jonathan <jonathangomezit@gmail.com>
2021-10-24 08:48:18 -0300

    colors and limits

--- src/script.js
+++ src/script.js
@@ -1,42 +1,62 @@
-((  
-    boxSize=60, //tamaño por cubo
-    amountBoxWidth=5, // ancho de matriz
-    amountBoxHeight=5 // altura de matriz
-) => {
+(async () => {
+    const body = document.querySelector('#body');
 
-const body = document.querySelector('#body');
+    const changesSize = () => body.style.height = `${window.innerHeight - 20}px`
 
-const changesSize = () => body.style.height = `${window.innerHeight}px`
+    addEventListener("DOMContentLoaded", changesSize);
+    addEventListener("resize", changesSize);
+    const fragment = new DocumentFragment();
 
-addEventListener("DOMContentLoaded", changesSize);
-addEventListener("resize", changesSize);
-
-const content = document.createElement("div")
-
-content.style.minWidth= `${boxSize * amountBoxWidth}px`;
-content.style.display="grid";
-content.style.gridTemplateColumns=`repeat(auto-fill, minmax(${boxSize}px, 1fr))`;
-content.style.gridAutoRows=`minmax(${boxSize}px, auto)`;
-
-const fragment = document.createDocumentFragment();
-
-for(let i = 0; i < amountBoxHeight; i++){
-    console.log('hola')
-    for(let j = 0; j < amountBoxWidth; j++) {
-
-        const box = document.createElement("div");
-    
-        box.className=`box box-${i + 1}-${j + 1}`
-        box.textContent=`${i + 1} - ${j + 1}`
+    const wait = () => {
+        const content = document.createElement("div")
+        content.textContent="esperando tus cuadritos..."
         
-        content.appendChild(box);
-        fragment.appendChild(content);
+        fragment.appendChild(content)
+        return fragment
     }
 
-}
+    const creationMatriz = (
+        boxSize=60, //tamaño por cubo
+        amountBoxWidth=10, // ancho de matriz
+        amountBoxHeight=10,// altura de matriz
+        color=true,
+    ) => {
+        const content = document.createElement("div")
+        if(amountBoxWidth <= 1000 && amountBoxHeight <= 1000) {
+            
+            content.style.width= `${boxSize * amountBoxWidth}px`;
+            content.style.display="grid";
+            content.style.gridTemplateColumns=`repeat(${amountBoxWidth}, ${boxSize}px)`;
+            content.style.gridAutoRows=`${boxSize}px`;
 
+            for(let i = 1; i <= amountBoxHeight; i++){
+                
+                for(let j = 1; j <= amountBoxWidth; j++) {
+                    console.log(`${i} - ${j}`)
+                    
+                    const box = document.createElement("div");
+                    box.className=`box box-${i}-${j}`
+                    if(boxSize >= 60 ) {
+                        box.textContent=`${i} - ${j}`
+                    }
+                    
+                    box.classList.add("box-green")
+                    
+                    content.appendChild(box);
+                    fragment.appendChild(content);
+                }
 
-
-body.appendChild(fragment)
+            }
+            
+            return fragment
+        }
+        content.textContent="Como maximo esta permitido hasta 1000 cubos de ancho y alto"
+        return fragment.appendChild(content)
+    }
+    
+    body.appendChild(wait())
+    setTimeout(() => {
+        body.replaceChild(creationMatriz(),body.querySelector('div'))
+    },100)
 
 })()
